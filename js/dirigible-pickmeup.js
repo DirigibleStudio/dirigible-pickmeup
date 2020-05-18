2d27ac571facf0c6c5839cc20a269ac62073a66c
Samuel Farrell <samuelfarrell@me.com>
2020-05-18 16:31:41 -0500

    Simplify

--- js/dirigible-pickmeup.js
+++ js/dirigible-pickmeup.js
@@ -1,4 +1,45 @@
 jQuery(document).ready(function ($) {
-  alert('heeeey there');
-  // 'ul#shipping_method'
+
+  $('ul#shipping_method input').each(function (index, value) {
+    var $this = $(this);
+    if ($this.val().indexOf("local_pickup") != -1) {
+      if ($this.prop('checked')) {
+        hideShippingFields();
+      }
+
+    }
+
+  });
+
+
+  $(document).on('change', 'ul#shipping_method input', function () {
+    var $this = $(this);
+    if ($this.val().indexOf("local_pickup") != -1) {
+      hideShippingFields();
+    } else {
+      showShippingFields();
+    }
+  });
+
+  function hideShippingFields() {
+    var $col1 = $('.col-1');
+    $('#ship-to-different-address-checkbox').prop('checked', false);
+    $('.woocommerce-shipping-fields').closest('.col-2').hide();
+    $('.woocommerce-additional-fields').appendTo($col1);
+    $col1.css({
+      marginLeft: "auto",
+      marginRight: "auto"
+    });
+  }
+
+  function showShippingFields() {
+    $col2 = $('.woocommerce-shipping-fields').closest('.col-2');
+    var $col1 = $('.col-1');
+    $('.woocommerce-additional-fields').appendTo($col2);
+    $col2.show();
+    $col1.css({
+      marginLeft: "",
+      marginRight: ""
+    });
+  }
 });
--- src/DirigiblePickmeup.php
+++ src/DirigiblePickmeup.php
@@ -8,26 +8,13 @@ class DirigiblePickmeup {
     $this->path = $args;
     $this->dir = plugin_dir_path($args); 
     $this->templates = "{$this->dir}src/templates";
-   
-   
-    add_action( 'customize_register', [ $this, 'registerCustomizer'], 999, 1 );
     add_action( 'wp_enqueue_scripts',  [ &$this, 'registerScripts' ] );
   }
-
 
   public function registerScripts() {
     wp_register_script( 'dirigible-pickmeup-js', plugins_url('dirigible-pickmeup/js/dirigible-pickmeup.js'), ['jquery'], NULL, true);
     wp_enqueue_script('dirigible-pickmeup-js');  
   }
-
-  function registerCustomizer($wp_customize) {
-    try {
-      $Settings = New DirigibleSettings($this->dir.'src/manifest.json');
-      $Settings->registerCustomizer($wp_customize);
-    }
-    catch (\Exception $e) { 
-    }
-  } 
 }
 
 ?>
